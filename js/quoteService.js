533e043816120f0207bc701636998aa030677e42
Jake Headley <jheadley@roomkey.com>
2017-03-28 16:50:06 -0600

    Refinements and stuff

--- js/quoteService.js
+++ js/quoteService.js
@@ -2,7 +2,7 @@ angular.module('quoteBooks').service('quoteService', function(){
 
   var quotes = [
     { text: 'It\'s my belief that history is a wheel. \"Inconsistency is my very essence\" -says the wheel- \"Rise up on my spokes if you like, but don\'t complain when you are cast back down into the depths. Good times pass away, but then so do the bad. Mutability is our tragedy, but it is also our hope. The worst of times, like the best, are always passing away\"', author: 'Boethius'},
-    { text: 'Life isn\'t about getting and having, it\'s about giving and being.', author: 'Kevin Kruse'},
+    { text: 'Life is not about getting and having, it is about giving and being.', author: 'Kevin Kruse'},
     { text: 'Whatever the mind of man can conceive and believe, it can achieve', author: 'Napoleon Hill'},
     { text: 'Strive not to be a success, but rather to be of value.', author: 'Albert Einstein'},
     { text: 'Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.', author: 'Robert Frost'},
